1f5b465bddda619569e7bbf3635565f261ebc3cd
Alec Johnson <alecjohnson55@gmail.com>
2017-05-10 18:13:37 -0700

    add route for getting nurse data from backend

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable global-require */
 import 'dotenv/config';
 import reactApp from './views/app';
-
+import getNurses from './api/nurses';
 const routes = (app) => {
 
   /* example api route */
-  app.get('/api/records', require('./api/records')); 
-
+  app.get('/api/records', require('./api/records'));
+  app.get('/api/nurses', getNurses);
   reactApp(app); // set up react routes
 };
 
